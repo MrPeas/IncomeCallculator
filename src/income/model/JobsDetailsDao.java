4a9d4197800d0fc3667d26efc9e0fce0860b7286
MrPeas <janusz.groszewski@gmail.com>
2016-11-13 14:27:16 +0100

    change name

--- src/income/model/JobsDetailsDao.java
+++ src/income/model/JobsDetailsDao.java
@@ -23,58 +23,58 @@ public class JobsDetailsDao {
         con = ConnectionManager.getConnection();
         try {
             Statement stmnt = con.createStatement();
-            ResultSet rs=stmnt.executeQuery("SELECT * FROM JOBS_DETAILS WHERE JOBS_DETAILS.ID_JOB="+jobsEntity.getId());
-            jobsDetails=setJobDetail(rs);
-            con.close();
-            return jobsDetails;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }finally {
-        }
-        return null;
-    }
-
-    public List<JobsDetailsEntity> findJobsDetailsByYear(JobsEntity job,int year){
-        this.jobsEntity = job;
-        List<JobsDetailsEntity> jobsDetails;
-        con = ConnectionManager.getConnection();
-        try {
-            Statement stmnt = con.createStatement();
-            ResultSet rs=stmnt.executeQuery("SELECT * FROM JOBS_DETAILS WHERE " +
-                    "JOBS_DETAILS.ID_JOB="+jobsEntity.getId()+" AND YEAR(JOBS_DETAILS.WOKR_DATE)="+year);
-            jobsDetails=setJobDetail(rs);
+            ResultSet rs = stmnt.executeQuery("SELECT * FROM JOBS_DETAILS WHERE JOBS_DETAILS.ID_JOB=" + jobsEntity.getId());
+            jobsDetails = getResult(rs);
             stmnt.close();
             con.close();
             return jobsDetails;
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
         }
         return null;
     }
 
-    public List<JobsDetailsEntity> findJobsDetailsByMonth(JobsEntity job,int month){
+    public List<JobsDetailsEntity> findJobsDetailsByYear(JobsEntity job, int year) {
         this.jobsEntity = job;
         List<JobsDetailsEntity> jobsDetails;
         con = ConnectionManager.getConnection();
         try {
             Statement stmnt = con.createStatement();
-            ResultSet rs=stmnt.executeQuery("SELECT * FROM JOBS_DETAILS WHERE " +
-                    "JOBS_DETAILS.ID_JOB="+jobsEntity.getId()+" AND MONTH(JOBS_DETAILS.WOKR_DATE)="+month);
-            jobsDetails=setJobDetail(rs);
+            ResultSet rs = stmnt.executeQuery("SELECT * FROM JOBS_DETAILS WHERE " +
+                    "JOBS_DETAILS.ID_JOB=" + jobsEntity.getId() + " AND YEAR(JOBS_DETAILS.WOKR_DATE)=" + year);
+            jobsDetails = getResult(rs);
             stmnt.close();
             con.close();
             return jobsDetails;
         } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
         }
         return null;
     }
 
-    private List<JobsDetailsEntity> setJobDetail(ResultSet rs) throws SQLException {
-        List<JobsDetailsEntity> jobsDetails=new ArrayList<>();
-        while(rs.next()){
+    public List<JobsDetailsEntity> findJobsDetailsByMonth(JobsEntity job, int month) {
+        this.jobsEntity = job;
+        List<JobsDetailsEntity> jobsDetails;
+        con = ConnectionManager.getConnection();
+        try {
+            Statement stmnt = con.createStatement();
+            ResultSet rs = stmnt.executeQuery("SELECT * FROM JOBS_DETAILS WHERE " +
+                    "JOBS_DETAILS.ID_JOB=" + jobsEntity.getId() + " AND MONTH(JOBS_DETAILS.WOKR_DATE)=" + month);
+            jobsDetails = getResult(rs);
+            stmnt.close();
+            con.close();
+            return jobsDetails;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        }
+        return null;
+    }
+
+    private List<JobsDetailsEntity> getResult(ResultSet rs) throws SQLException {
+        List<JobsDetailsEntity> jobsDetails = new ArrayList<>();
+        while (rs.next()) {
             JobsDetailsEntity temp = new JobsDetailsEntity();
             temp.setId(rs.getLong("id"));
             temp.setHours(rs.getDouble("hours"));
